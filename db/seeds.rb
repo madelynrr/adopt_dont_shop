36d62ea5279e1b50ee8870e37e95adf0f16c5373
Madelyn Romero <m.romero013@gmail.com>
2019-11-24 18:56:25 -0700

    Add two pets

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,6 @@ shelter_2 = Shelter.create(name: "Denver Shelter", address: "456 Regular Ave.", 
 shelter_3 = Shelter.create(name: "Longmont Shelter", address: "456 Main St.", city: "Longmont", state: "CO", zip: "80110")
 shelter_4 = Shelter.create(name: "Lafayette Shelter", address: "123 Regular St.", city: "Boulder", state: "CO", zip: "80303")
 shelter_5 = Shelter.create(name: "Englewood Shelter", address: "3075 W. Floyd Ave.", city: "Englewood", state: "CO", zip: "80110")
+
+pet_1 = shelter_1.pets.create(image: "https://www.thehappycatsite.com/wp-content/uploads/2017/05/grey4.jpg", name: "Faye", description: "", age: 3, sex: "Female", adoption_status: "Available for Adoption")
+pet_2 = shelter_2.pets.create(image: "https://image.shutterstock.com/image-photo/cute-muzzle-black-cat-closeup-260nw-525137104.jpg", name: "Ed", description: "", age: 2, sex: "Male", adoption_status: "Available for Adoption")
