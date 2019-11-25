73719e8505154e2a3f1387136d52468ca189f15a
Madelyn Romero <m.romero013@gmail.com>
2019-11-25 15:00:30 -0700

    Refactor delete shelter test to account for pets in shelter

--- spec/features/shelters/user_can_delete_individual_shelter_spec.rb
+++ spec/features/shelters/user_can_delete_individual_shelter_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe "as a user" do
 
     shelter = Shelter.create(name: "Boulder Shelter", address: "123 Main St.", city: "Boulder", state: "CO", zip: "80303")
 
+    pet = shelter.pets.create(image: "https://c8.alamy.com/comp/S0B5EF/goofy-cat-S0B5EF.jpg", name: "Fay", description: "", age: 5, sex: "Male", adoption_status: true)
+
     visit "shelters/#{shelter.id}"
 
     click_link "Delete Shelter"
