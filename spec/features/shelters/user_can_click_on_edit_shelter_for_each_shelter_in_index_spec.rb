92d4c88b778b1f7aeaf4e301095f96ee03e9ea24
Madelyn Romero <m.romero013@gmail.com>
2019-11-26 13:25:13 -0700

    Create test for edit buttons next to every shelter on index page

--- spec/features/shelters/user_can_click_on_edit_shelter_for_each_shelter_in_index_spec.rb
+++ spec/features/shelters/user_can_click_on_edit_shelter_for_each_shelter_in_index_spec.rb
@@ -1,17 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "user can click on edit for each shelter in index" do
-  xit "redirects to shelter edit page" do
+  it "redirects to shelter edit page" do
     shelter_1 = Shelter.create(name: "Boulder Shelter", address: "123 Main St.", city: "Boulder", state: "CO", zip: "80303")
     shelter_2 = Shelter.create(name: "Denver Shelter", address: "456 Regular Ave.", city: "Denver", state: "CO", zip: "80120")
     shelter_3 = Shelter.create(name: "Longmont Shelter", address: "456 Main St.", city: "Longmont", state: "CO", zip: "80110")
 
-    visit "/shelters"
+    visit '/shelters'
 
-    click_link "Edit Shelter"
+    click_link "Edit Boulder Shelter"
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/edit")
+    expect(page).to have_content("Edit #{shelter_1.name} Information")
 
-    expect(current_path).to eq("/shelters/:shelter_id")
+    visit '/shelters'
+    click_link "Edit Denver Shelter"
+    expect(current_path).to eq("/shelters/#{shelter_2.id}/edit")
+    expect(page).to have_content("Edit #{shelter_2.name} Information")
 
-    expect(page).to have_content("Name: #{shelter.name}")
+    visit '/shelters'
+    click_link "Edit Longmont Shelter"
+    expect(current_path).to eq("/shelters/#{shelter_3.id}/edit")
+    expect(page).to have_content("Edit #{shelter_3.name} Information")
   end
 end
