5713d7473ae3d2f995f808aa5ffd57471cee7197
Madelyn Romero <m.romero013@gmail.com>
2019-11-26 18:41:35 -0700

    Create test to delete pet profile from shelter's pet index page

---
+++ spec/features/pets/user_can_delete_pet_profile_from_shelter_pet_index_page_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "user can click link on shelter's pet index page" do
+  it "will delete pet profile and redirect to shelter's pet index page" do
+    shelter_1 = Shelter.create(name: "Boulder Shelter", address: "123 Main St.", city: "Boulder", state: "CO", zip: "80303")
+
+    pet_1 = shelter_1.pets.create(image: "https://www.thehappycatsite.com/wp-content/uploads/2017/05/grey4.jpg", name: "Faye", description: "", age: 3, sex: "Female", adoption_status: "Available for Adoption")
+    pet_2 = shelter_1.pets.create(image: "https://image.shutterstock.com/image-photo/cute-muzzle-black-cat-closeup-260nw-525137104.jpg", name: "Ed", description: "", age: 2, sex: "Male", adoption_status: "Available for Adoption")
+
+    visit "/shelters/#{shelter_1.id}/pets"
+
+    click_link "Delete #{pet_1.name}'s Profile"
+
+    expect(current_path).to eq('/pets')
+    expect(page).to_not have_css("img[src*='https://www.thehappycatsite.com/wp-content/uploads/2017/05/grey4.jpg']")
+    expect(page).to_not have_content("Name: Faye")
+    expect(page).to_not have_content("Age: 3")
+    expect(page).to_not have_content("Sex: Female")
+  end
+end
