ff39e7f376c6fb24a59fc32d1e1cc54e17a70ad8
Madelyn Romero <m.romero013@gmail.com>
2019-11-26 19:02:18 -0700

    Add tests to link shelter name to shelter show page for shelter index page and shelter pet index page

--- spec/features/shelters/user_can_click_shelter_name_to_go_to_shelter_show_page_spec.rb
+++ spec/features/shelters/user_can_click_shelter_name_to_go_to_shelter_show_page_spec.rb
@@ -8,8 +8,32 @@ RSpec.describe "a user can click on a shelter name" do
       visit "/shelters/#{shelter_1.id}/edit"
 
       click_link "Boulder Shelter"
+      expect(current_path).to eq("/shelters/#{shelter_1.id}")
+    end
+
+    it "clicks shelter name in index view" do
+      shelter_1 = Shelter.create(name: "Boulder Shelter", address: "123 Main St.", city: "Boulder", state: "CO", zip: "80303")
+
+      visit '/shelters'
+
+      click_link "Boulder Shelter"
+      expect(current_path).to eq("/shelters/#{shelter_1.id}")
+    end
+
+    it "clicks shelter name in pet index page" do
+      shelter_1 = Shelter.create(name: "Boulder Shelter", address: "123 Main St.", city: "Boulder", state: "CO", zip: "80303")
+      pet_1 = shelter_1.pets.create(image: "https://www.thehappycatsite.com/wp-content/uploads/2017/05/grey4.jpg", name: "Bitty", description: "", age: 3, sex: "Female", adoption_status: "Available for Adoption")
+
+      visit "/shelters/#{shelter_1.id}/pets"
+
+      click_link "Boulder Shelter"
 
       expect(current_path).to eq("/shelters/#{shelter_1.id}")
     end
+
+
+
+
+
   end
 end
